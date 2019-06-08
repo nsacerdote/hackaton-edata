d90c82b020e751b3b92f8076290b61d177508eae
Natalio Sacerdote <natalio.sacerdote@edataconsulting.de>
2019-06-08 15:44:22 +0100

    Add Level and Scene, use it on Game, nextScene on UI

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Pos } from './model/pos';
 import { CellActor } from './model/cell.actor';
+import { Level } from './model/level';
+import { Scene } from './model/scene';
 
 @Component({
   selector: 'app-game',
@@ -9,14 +11,43 @@ import { CellActor } from './model/cell.actor';
 })
 export class GameComponent implements OnInit {
 
-  actors = [
-    new CellActor(new Pos(300, 200), true),
-    new CellActor(new Pos(100, 100), false),
-  ];
+  private levels: Record<string, Level> = {
+    INTRO: {
+      scenes: [
+        {
+          background: '/assets/images/background.png',
+          actors: [
+            new CellActor(new Pos(300, 200), true),
+            new CellActor(new Pos(100, 100), false)
+          ]
+        },
+        {
+          background: '/assets/images/background.png',
+          actors: [
+            new CellActor(new Pos(200, 200), true),
+            new CellActor(new Pos(500, 100), false)
+          ]
+        }
+      ]
+    }
+  };
 
-  constructor() { }
+  level: Level = null;
+  currentScene: Scene = null;
+
+  constructor() {}
 
   ngOnInit() {
+    this.loadIntroLevel();
   }
 
+  private loadIntroLevel() {
+    this.level = this.levels.INTRO;
+    this.currentScene = this.level.scenes[0];
+  }
+
+  loadNextScene() {
+    const nextIndex = this.level.scenes.indexOf(this.currentScene) + 1;
+    this.currentScene = this.level.scenes[nextIndex];
+  }
 }
--- frontend/src/app/game/ui/ui.component.ts
+++ frontend/src/app/game/ui/ui.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-ui',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UiComponent implements OnInit {
 
+  @Output() nextSceneRequest = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(
+      () => this.nextSceneRequest.emit(),
+      2000
+    );
   }
 
 }
