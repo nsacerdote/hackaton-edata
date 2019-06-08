ac9a0033f9d2d44d1b445ca714caa5313fc8ac33
Natalio Sacerdote <natalio.sacerdote@edataconsulting.de>
2019-06-08 21:11:14 +0100

    Fix bug with face avatar in dialog

--- frontend/src/app/game/model/dialog.ts
+++ frontend/src/app/game/model/dialog.ts
@@ -1,5 +1,3 @@
-import {Optional} from '@angular/core';
-
 export class Dialog {
   constructor(public text: string, public audioFile: string, public faceFile: string, public promptUser: boolean = false) {
   }
