9b79ee049dfb4ba4bdd200dccbc25c67aebb6bc1
EDATA\jborges <jose.borges@edataconsulting.de>
2019-06-08 11:30:45 +0100

    Added core button

--- frontend/src/app/core/button/button.component.ts
+++ frontend/src/app/core/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ButtonComponent implements OnInit {
 
   @Input() text: string;
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter();
 
   constructor() { }
 
